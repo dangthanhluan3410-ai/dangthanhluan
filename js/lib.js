555862b826f192513b4df27e04e60e6ebc267cfa
dang thanh luan <dangthanhluan3410@gmail.com>
2026-06-04 10:35:02 +0700

    Luan

--- js/lib.js
+++ js/lib.js
@@ -1,1 +1,14 @@
-js hnfdyhjxf
+/*viết mã javascript*/
+function printBCC(num)
+{
+    let result = "";
+
+    let i = 1;
+    while(i<=10)
+    {
+        result += `${num} x ${i} = ${num*i} <br>`;
+        i++;
+    }
+    //đưa kết quả vào thẻ div có id là "result"
+    doccument.getElementbyid("result").innerHTML= result;
+}
